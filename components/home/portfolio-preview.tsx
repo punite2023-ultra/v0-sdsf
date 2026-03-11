c8776c6bea06e801d7ea37e1938b7ac18a7da0ad
punite2023-ultra <punite2023@gmail.com>
2026-03-11 09:36:26 +0800

    Update portfolio-preview.tsx

--- components/home/portfolio-preview.tsx
+++ components/home/portfolio-preview.tsx
@@ -11,10 +11,10 @@ const categories = ["All", "Branding", "Web Design", "Development", "Marketing"]
 const projects = [
   {
     id: 1,
-    title: "Luxe Fashion Brandss",
-    category: "Branding",
+    title: "GARNIER: The Unfiltered Glow Campaign",
+    category: "Content Creation & Branding",
     image:
-      "https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=800&h=600&fit=crop",
+      "https://images.unsplash.com/photo-1582020738577-2e7a48043902?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGxvdGlvbiUyMHByb2R1Y3RzfGVufDB8fDB8fHww?w=800&h=600&fit=crop",
     href: "/portfolio/luxe-fashion",
   },
   {
