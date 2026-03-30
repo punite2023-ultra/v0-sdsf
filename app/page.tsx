ed77581f70f9cd117e55f089db871b62abf3065f
punite2023-ultra <punite2023@gmail.com>
2026-03-31 07:31:43 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
     <>
       <Navigation />
       <main>
-        <HeroSection />
+        <HeroSectionSimple />
         <ClientsMarquee />
         <AboutPreview />
         <ServicesPreview />
