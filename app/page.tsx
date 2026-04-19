31bd711e572275b7c3b8f78d93f7e8de872d18e8
punite2023-ultra <punite2023@gmail.com>
2026-04-20 06:04:08 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import { Navigation } from "@/components/navigation"
 import { SiteFooter } from "@/components/site-footer"
 import { HeroSectionSimple } from "@/components/home/hero-section-simple"
-import { ServicesPreview } from "@/components/home/services-preview"
+import { ClientsMarquee } from "@/components/home/clients-marquee"
 import { AboutPreview } from "@/components/home/about-preview"
-import { PortfolioPreview } from "@/components/home/portfolio-preview"
+import { ServicesPreview } from "@/components/home/services-preview"
 import { StatsSection } from "@/components/home/stats-section"
+import { PortfolioPreview } from "@/components/home/portfolio-preview"
 import { TestimonialsSection } from "@/components/home/testimonials-section"
 import { BlogPreview } from "@/components/home/blog-preview"
-import { ClientsMarquee } from "@/components/home/clients-marquee"
 
 export default function HomePage() {
   return (
@@ -16,17 +16,16 @@ export default function HomePage() {
 
       <main>
         <HeroSectionSimple />
-
-        {/* <ClientsMarquee /> */}
-        {/* <AboutPreview /> */}
-        {/* <ServicesPreview /> */}
-        {/* <PortfolioPreview /> */}
-        {/* <StatsSection /> */}
-        {/* <TestimonialsSection /> */}
-        {/* <BlogPreview /> */}
+        <ClientsMarquee />
+        <AboutPreview />
+        <ServicesPreview />
+        <StatsSection />
+        <PortfolioPreview />
+        <TestimonialsSection />
+        <BlogPreview />
       </main>
 
-      {/* <SiteFooter /> */}
+      <SiteFooter />
     </>
   )
 }
