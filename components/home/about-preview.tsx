2ea0d9c72f365b08b5f297be45eaac7f145c60f2
punite2023-ultra <punite2023@gmail.com>
2026-04-21 15:41:28 +0800

    Update about-preview.tsx

--- components/home/about-preview.tsx
+++ components/home/about-preview.tsx
@@ -54,7 +54,7 @@ export function AboutPreview() {
                 <div className="space-y-4">
                   <div className="img-zoom overflow-hidden rounded-2xl">
                     <Image
-                      src="https://images.unsplash.com/photo-1600880292203-757bb62b4baf?w=400&h=500&fit=crop"
+                      src="https://unsplash.com/photos/black-smartphone-taking-photo-of-yellow-round-fruits-8zsBofKrhP8=400&h=500&fit=crop"
                       alt="Team meeting"
                       width={400}
                       height={500}
